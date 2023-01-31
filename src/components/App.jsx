13f6f36fdf8786369f95acf9ad6e54b064b94cca
kszczerbowski <kszczerbowski223@gmail.com>
2023-01-31 13:19:46 +0100

    inline div styling removed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import css from './App.module.css';
 import React from 'react';
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
@@ -9,13 +10,7 @@ import friends from './FriendList/friends.json';
 import transactions from './TransactionHistory/transactions.json';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div className={css.container}>
       <Profile
         username={user.username}
         tag={user.tag}
